9d488be648059fdfad8e7b194d1bc5fbd809bd49
fanqh <fanqinghua@actnova.com>
2014-11-05 09:54:51 +0800

    init my project from usb host msc

---
+++ Project/stm32f4_hostAOA/README.md
@@ -0,0 +1,4 @@
+stm32f4_hostAOA
+===============
+
+host aoa 
---
+++ Project/stm32f4_hostAOA/inc/usart.h
@@ -0,0 +1,49 @@
+#ifndef _USART_H
+#define _USART_H
+
+#include <stdio.h>
+#include "stm32f4xx.h"
+
+/**
+ * @brief Definition for COM port1, connected to USART3
+ */  	/*
+#define Open_USART                        USART2
+#define Open_USART_CLK                    RCC_APB1Periph_USART2
+
+#define Open_USART_TX_PIN                 GPIO_Pin_2
+#define Open_USART_TX_GPIO_PORT           GPIOA
+#define Open_USART_TX_GPIO_CLK            RCC_AHB1Periph_GPIOA
+#define Open_USART_TX_SOURCE              GPIO_PinSource2
+#define Open_USART_TX_AF                  GPIO_AF_USART2
+
+
+#define Open_USART_RX_PIN                 GPIO_Pin_6
+#define Open_USART_RX_GPIO_PORT           GPIOD
+#define Open_USART_RX_GPIO_CLK            RCC_AHB1Periph_GPIOD
+#define Open_USART_RX_SOURCE              GPIO_PinSource6
+#define Open_USART_RX_AF                  GPIO_AF_USART2
+
+#define Open_USART_IRQn                   USART2_IRQn	 
+*/
+#define Open_USART                        USART1
+#define Open_USART_CLK                    RCC_APB2Periph_USART1
+
+#define Open_USART_TX_PIN                 GPIO_Pin_9
+#define Open_USART_TX_GPIO_PORT           GPIOA
+#define Open_USART_TX_GPIO_CLK            RCC_AHB1Periph_GPIOA
+#define Open_USART_TX_SOURCE              GPIO_PinSource9
+#define Open_USART_TX_AF                  GPIO_AF_USART1
+
+
+#define Open_USART_RX_PIN                 GPIO_Pin_10
+#define Open_USART_RX_GPIO_PORT           GPIOA
+#define Open_USART_RX_GPIO_CLK            RCC_AHB1Periph_GPIOA
+#define Open_USART_RX_SOURCE              GPIO_PinSource10
+#define Open_USART_RX_AF                  GPIO_AF_USART1
+
+#define Open_USART_IRQn                   USART1_IRQn	   
+
+void USART_Configuration(void);
+void USART_NVIC_Config(void);
+
+#endif /*_USART_H*/
